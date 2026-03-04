01fd40e79b566f66bdf3fcfeb52fb6464c1dfbfc
Bornunique911 <69379200+Bornunique911@users.noreply.github.com>
2026-03-04 12:49:44 +0530

    Fix formatting and add newline at end of elf.go

--- objfile/elf.go
+++ objfile/elf.go
@@ -354,9 +354,10 @@ scan:
 					offset += moduledata_idx + len(pclntabVA_bytes)
 					continue
 				}
+			}
 
-				found = true
-				break scan
+			found = true
+			break scan
 			} else {
 				break
 			}
